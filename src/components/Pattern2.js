bc8d138c8ff064b08814c8da581d69242bb8c727
LucFERRO <luchenri.ferro@gmail.com>
2022-09-09 11:17:19 +0200

    display none on spots that are hidden

--- src/components/Pattern2.js
+++ src/components/Pattern2.js
@@ -15,14 +15,14 @@ export default function Pattern2({positions,random}) {
 
     return (
         <div className="container2">
-            <div style={{ '--i': 0 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':0, background: positions[0] ? `${placement} ${vykas}` : '' }}></span></div>
-            <div style={{ '--i': 1 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':1, background: positions[1] ? `${placement} ${vykas}` : '' }}></span></div>
-            <div style={{ '--i': 2 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':2, background: positions[2] ? `${placement} ${vykas}` : '' }}></span></div>
-            <div style={{ '--i': 3 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':3, background: positions[3] ? `${placement} ${vykas}` : '' }}></span></div>
-            <div style={{ '--i': 4 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':4, background: positions[4] ? `${placement} ${vykas}` : '' }}></span></div>
-            <div style={{ '--i': 5 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':5, background: positions[5] ? `${placement} ${vykas}` : '' }}></span></div>
-            <div style={{ '--i': 6 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':6, background: positions[6] ? `${placement} ${vykas}` : '' }}></span></div>
-            <div style={{ '--i': 7 }}><span className="position2" onClick={clickSafeSpot} style={{'--i':7, background: positions[7] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 0 }}><span className={`position2 ${ positions[0]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':0, background: positions[0] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 1 }}><span className={`position2 ${ positions[1]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':1, background: positions[1] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 2 }}><span className={`position2 ${ positions[2]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':2, background: positions[2] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 3 }}><span className={`position2 ${ positions[3]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':3, background: positions[3] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 4 }}><span className={`position2 ${ positions[4]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':4, background: positions[4] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 5 }}><span className={`position2 ${ positions[5]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':5, background: positions[5] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 6 }}><span className={`position2 ${ positions[6]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':6, background: positions[6] ? `${placement} ${vykas}` : '' }}></span></div>
+            <div style={{ '--i': 7 }}><span className={`position2 ${ positions[7]? '' : 'ninja'}`} onClick={clickSafeSpot} style={{'--i':7, background: positions[7] ? `${placement} ${vykas}` : '' }}></span></div>
         </div>
     )
 }
