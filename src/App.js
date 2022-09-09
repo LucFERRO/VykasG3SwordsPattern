06dfd4b68ee633a3996b88988eaa3a775c509ea0
LucFERRO <luchenri.ferro@gmail.com>
2022-09-09 13:12:53 +0200

    position object + safe pattern as prop

--- src/App.js
+++ src/App.js
@@ -17,18 +17,17 @@ function App(props) {
     ]
 
     const patternListObj = [
-        {pattern: [false,true,false,true,true,true,false,true], safe: 5 },
-        {pattern: [true,false,true,false,true,false,true,true], safe: 7 },
-        {pattern: [true,true,false,true,false,true,false,true], safe: 0 },
-        {pattern: [false,false,true,false,true,true,true,true], safe: 6 },
-        {pattern: [true,false,false,true,true,false,true,true], safe: 7 },
-        {pattern: [false,true,true,true,true,false,false,true], safe: 4 },
-        {pattern: [true,true,false,true,true,false,false,true], safe: 4 },
-        {pattern: [false,true,true,false,true,false,true,true], safe: 1 },
-        {pattern: [false,true,true,false,true,false,true,true], safe: 2 }
+        {pattern: [false,true,false,true,true,true,false,true], safe: [5] },
+        {pattern: [true,false,true,false,true,false,true,true], safe: [7] },
+        {pattern: [true,true,false,true,false,true,false,true], safe: [0] },
+        {pattern: [false,false,true,false,true,true,true,true], safe: [6] },
+        {pattern: [true,false,false,true,true,false,true,true], safe: [7] },
+        {pattern: [false,true,true,true,true,false,false,true], safe: [4] },
+        {pattern: [true,true,false,true,true,false,false,true], safe: [4] },
+        {pattern: [false,true,true,false,true,false,true,true], safe: [1,2] }
     ]
 
-    const [position, setPosition] = useState(() => {return [true,true,true,true,true,true,true,true]})
+    const [position, setPosition] = useState(() => {return {pattern: [true,true,true,true,true,true,true,true], safe:[0,1,2,3,4,5,6,7]}})
     const [showButton, setShowButton] = useState(true);
 
     const randomPattern = () => {
@@ -40,14 +39,24 @@ function App(props) {
         return newPattern
     }
 
+    const randomPatternObj = () => {
+        let randNum = Math.floor(Math.random()*patternListObj.length)
+        let newPattern = patternListObj[randNum].pattern
+        let safeSpots = patternListObj[randNum].safe
+        while (JSON.stringify(newPattern)==JSON.stringify(position)) {
+            newPattern = patternListObj[Math.floor(Math.random()*patternListObj.length)]
+        }
+        return {pattern: newPattern, safe: safeSpots}
+    }
+
     const random = () => {
-        setPosition(randomPattern())
+        setPosition(randomPatternObj())
     }
 
     return (
         <div id="home">
             { showButton ? <button onClick={() => {random(); setShowButton(false)}}>Start</button> : <p></p>}
-            <Pattern2 positions={position} random={random} />
+            <Pattern2 positions={position.pattern} safeSpots={position.safe} random={random} />
         </div>
     )
 }
--- src/components/Pattern2.js
+++ src/components/Pattern2.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Vykas from '../assets/img/VykasPortrait.png'
 import Mine from '../assets/img/Mine.jpg'
 
-export default function Pattern2({positions,random}) {
+export default function Pattern2({positions,random,safeSpots}) {
 
     let vykas = 'url("https://images-ext-1.discordapp.net/external/R9PKrKA0hCp8dOVoDD_bX1Z1WOmsxkBG2KSd3N9T8ww/%3Fcb%3D20220216075400/https/static.wikia.nocookie.net/lostark_gamepedia/images/4/4d/Vykas_Card_Portrait.png/revision/latest/top-crop/width/360/height/360")'
     let placement = '50% 35%'
+
+    console.log(safeSpots)
 
     const clickSafeSpot = (e) => {
         e.preventDefault()
